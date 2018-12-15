ff53c894f7f6d9b6eecf4d701b1efd91c62c643f
bluecjiang <bluecjiang@tencent.com>
2018-12-15 19:47:50 +0800

    feat: 添加创建作品弹框

---
+++ src/components/Pop.vue
@@ -0,0 +1,51 @@
+<template>
+ <modal title="创建作品" class="modal">
+   <div class="container">
+     <div class="form-row">
+       <label class="form-label">作品名称</label>
+       <input class="form-input" v-model="form.name"/>
+     </div>
+     <div class="form-row">
+       <label class="form-label">作品描述</label>
+       <input class="form-input" v-model="form.desc"/>
+     </div>
+   </div>
+ </modal>
+</template>
+
+<script>
+export default {
+  data: function(){
+    return {
+      form: {
+        name: '',
+        desc: ''
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.container{
+  border-top: 1px solid #ccc;
+}
+
+.form-row{
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid #ccc;
+}
+
+.form-label{
+
+}
+
+.form-input{
+  
+}
+</style>
+
+
+
+
